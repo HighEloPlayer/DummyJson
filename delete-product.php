3ba334c8468acfc61e6672e8811a909cbb2c607b
highEloPlayer <nfcobra20@gmail.com>
2022-10-04 16:26:24 +0800

    First commit

---
+++ delete-product.php
@@ -0,0 +1,67 @@
+<?php
+require "vendor/autoload.php";
+
+use GuzzleHttp\Client;
+
+$client = new Client([
+        'base_uri' => 'https://dummyjson.com/'
+]);
+
+$products = [
+	'json' => ['id' => '3',
+  'title' => 'Iphone13 Max',
+  'description' => 'The iPhone 13 Pro Max is Apples biggest phone in the lineup with a massive, 6.7" screen that for the first time in an iPhone comes with 120Hz ProMotion display that ensures super smooth scrolling. The benefit of such a gigantic phone is that it also comes with the biggest battery of all iPhone 13 series.',
+  'price' => '20000',
+  'brand' => 'Apple',
+  'category' => 'Smartphones',
+  'thumbnail' => 'thumbnail.jpg'
+	]
+];
+
+
+$response = $client->delete('https://dummyjson.com/products/1');
+$code = $response->getStatusCode();
+$body = $response->getBody();
+$product = json_decode($body);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Delete Product</title>
+</head>
+<body>
+<div class = "container"> 
+        <table class="table table-striped">
+                <thead>
+                        <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Title</th>
+                                <th scope="col">Description</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Brand</th>
+                                <th scope="col">Category</th>
+                                <th scope="col">Thumbnail</th>
+                        </tr>
+                </thead>
+        <tbody>
+                <tr>
+                <th scope="row" href="single-product.php"><?= $product->id ?></th>
+                <td><?= $product->title?></td>
+                <td><?= $product->description?></td>
+                <td><?= $product->price?></td>
+                <td><?= $product->brand?></td>
+                <td><?= $product->category?></td>
+                <td><img src="<?= $product->thumbnail?>"></td>
+                </tr>
+        </tbody>
+</table>
+</body>
+</html>
